e7e710c340e98bf1e77696bbd2adbcd5cd8e7cd1
Rishabh Rao <71926015+rishabhrao12@users.noreply.github.com>
2024-06-19 09:30:54 +0530

    multi head attention

--- nanoGPT/bigram.py
+++ nanoGPT/bigram.py
@@ -3,19 +3,22 @@ import torch.nn as nn
 from torch.nn import functional as F 
 
 # hyperparameters
-batch_size = 4  # how many independent sequences will be processed in parallel?
-block_size = 8  # what is the maximum context length for prediction?
+batch_size = 64  # how many independent sequences will be processed in parallel?
+block_size = 256  # what is the maximum context length for prediction?
 max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-3
+eval_interval = 500
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 # ---------------
 
 torch.manual_seed(1337)
 
-with open('input.txt', 'r', encoding='utf-8') as file:
+with open('/Users/rishabhrao/Documents/GitHub/deep-learning/nanoGPT/input.txt', 'r', encoding='utf-8') as file:
   text = file.read()
 
 # Unqiue characters in the text
@@ -25,8 +28,8 @@ vocab_size = len(chars)
 # Create mapping for characters
 itos = {i:s for i, s in enumerate(chars)}
 stoi = {s:i for i, s in enumerate(chars)}
-encode = lambda s: [stoi[c] for c in s] # take a string output a list of integer
-decode = lambda l: ''.join([itos[i] for i in l]) # take a list of integer output a string
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string output a list of integer
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integer output a string
 
 data = torch.tensor(encode(text), dtype=torch.long)
 n = int(0.9 * len(data))
@@ -36,9 +39,10 @@ val_data = data[n:]
 def get_batch(split):
   # generate small batch of data of inputs x and target y
   data = train_data if split == 'train' else val_data
-  ix = torch.randint(len(train_data) - block_size, (batch_size, ))
+  ix = torch.randint(len(data) - block_size, (batch_size, ))
   x = torch.stack([data[i:i+block_size] for i in ix])
   y = torch.stack([data[i+1: i+block_size+1] for i in ix])
+  x, y = x.to(device), y.to(device)
   return x, y
 
 @torch.no_grad()
@@ -64,6 +68,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -73,29 +78,78 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2, -1) * C**-0.5     # (B, T, C) @ (B, C, T) --> (B, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))    # (B, T, T)
         wei = F.softmax(wei, dim=-1)    # (B, T, T)
+        wei = self.dropout(wei)
         # perform weighted aggregation of values
         v = self.value(x)
         out = wei @ v   # (B, T, T) @ (B, T, C) --> (B, T, C)
         return out
 
+class MultiHeadAttention(nn.Module):
+    """multiple heads of self attention in parallel"""
+
+    def __init__(self, num_heads, head_size) -> None:
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+
+class FeedForward(nn.Module):
+    """a simple linear layer followed by a non-linearity"""
+
+    def __init__(self, n_embd) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head) -> None:
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+
+
 class BigramLanguageModel(nn.Module):
 
     def __init__(self) -> None:
         super().__init__()
+        # each token directly reads of the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.poition_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
-        B, T = idx.shape
+        B, T = idx.shape 
 
         # idx and targets are both (B, T) tensors of integers
         token_emb = self.token_embedding_table(idx) # (B, T, C)
         pos_emb = self.poition_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = token_emb + pos_emb # (B, T, C)
-        x = self.sa_head(x)
-        logits = self.lm_head(x) 
+        x = self.blocks(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
 
         if targets is None:
             loss = None
@@ -133,9 +187,9 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 for iter in range(max_iters):
 
     # every once in a while evaluate loss on train and val split
-    if iter % eval.interval == 0:
+    if iter % eval_interval == 0:
         losses = estimate_loss()
-        print(f'Step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}')
+        print(f"Step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
     # sample a batch of data
     xb, yb = get_batch('train')
